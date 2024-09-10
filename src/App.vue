d008d26b74a698f920869ae7526b44a6a1720411
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-09-10 16:24:06 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  
+  <router-view></router-view>
+  <!-- <HomePage msg="Vite + Vue" /> -->
+</template>
+
+<style lang="scss">
+
+</style>
